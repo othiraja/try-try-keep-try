38ff2698806f8c97c0a5a4a882da18b5870541df
othiraja <83268302+othiraja@users.noreply.github.com>
2021-12-12 21:54:49 +0530

    ❤️ With name

---
+++ love .py
@@ -0,0 +1,23 @@
+
+
+from turtle import *
+color ('red')
+begin_fill()
+pensize(3)
+left (50)
+forward(150)
+circle ( 50,200)
+right (145)
+circle( 50,200)
+forward (133)
+import turtle
+turtle.goto(1,1)
+turtle.color("pink")
+turtle.pendown()
+turtle.forward(25)
+turtle.penup()
+turtle.write("sufiyan")
+end_fill()
+
+
+
